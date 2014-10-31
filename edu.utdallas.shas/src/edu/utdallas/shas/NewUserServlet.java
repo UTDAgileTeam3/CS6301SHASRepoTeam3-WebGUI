0669052b82831ac31c1c1d9a1a6c307a01b23457
ngdebusk <ngdebusk@cometnet-10-21-27-239.utdallas.edu>
2014-10-31 14:23:12 -0500

    Changes to New User Servlet

--- edu.utdallas.shas/src/edu/utdallas/shas/NewUserServlet.java
+++ edu.utdallas.shas/src/edu/utdallas/shas/NewUserServlet.java
@@ -1,6 +1,12 @@
 package edu.utdallas.shas;
 
+import java.sql.Statement;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -20,10 +26,33 @@ public class NewUserServlet extends HttpServlet {
 		String email = req.getParameter("new_phonenumber");
 		String phonenumber = req.getParameter("new_email");
 		
+		Properties properties = new Properties();
+		properties.load(getServletContext().getResourceAsStream("/WEB-INF/database.properties"));
+		System.out.println(properties);
+		String url = properties.getProperty("jdbc.url");
+		String driver = properties.getProperty("jdbc.driver");
+		String username = properties.getProperty("jdbc.username");
+		String password = properties.getProperty("jdbc.password");
+
+		try {
+			Class.forName(driver);
+			Connection myConn=DriverManager.getConnection(url, username, password);
+			Statement statement = myConn.createStatement() ;
+			ResultSet resultset = statement.executeQuery("insert into shasuser ()") ;
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
 		System.out.println("User info::: ");
 		System.out.println("Name: "+name+"\nUname: "+newusername+"\npwd: "+newpassword+"\n confirm: "+confirmpassword+"\n email: "+email+"\n phone: "+phonenumber);
 		RequestDispatcher rd = req.getRequestDispatcher("./newUser.jsp");
 		rd.forward(req, resp);
+		
+		
 	}
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
